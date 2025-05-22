b7b6b2ab2833fb077f4ce769bb27a569d78ee0db
patinodeveloper <pepe_toni_02@hotmail.com>
2025-05-22 13:12:36 -0600

    Welcome text added

--- src/app/core/pages/home/home.component.ts
+++ src/app/core/pages/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CardModule, ButtonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
